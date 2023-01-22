abfba8f3cd6013a6ab9a6993e40a4e1d0a6a47ac
Guo Yunhe <i@guoyunhe.me>
2023-01-22 14:17:19 +0800

    basic file download and redirect handling

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,25 @@
+import { existsSync, rmSync } from 'fs';
+import { join } from 'path';
 import { download } from '.';
 
 describe('download()', () => {
   it('download file', async () => {
-    await download('http://redump.org/datfile/ps2/serial,version,disc', 'tmp/ps2.zip');
+    const url = 'http://redump.org/datfile/ps2/serial,version,disc';
+    const dist = join('tmp', 'download', 'data.zip');
+    if (existsSync(dist)) {
+      rmSync(dist, { force: true });
+    }
+    await download(url, dist);
+    expect(existsSync(dist)).toBeTruthy();
+  });
+
+  it('handle redirect', async () => {
+    const url = 'https://source.unsplash.com/random';
+    const dist = join('tmp', 'redirect', 'random.jpg');
+    if (existsSync(dist)) {
+      rmSync(dist, { force: true });
+    }
+    await download(url, dist);
+    expect(existsSync(dist)).toBeTruthy();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,29 @@ import http from 'http';
 import https from 'https';
 import { dirname } from 'path';
 
-export async function download(url: string, dist: string) {
-  const file = createWriteStream(dist);
-  await mkdir(dirname(dist), { recursive: true });
-  const get = url.startsWith('https://') ? https.get : http.get;
-  get(url, (res) => {
-    res.pipe(file, { end: false });
-    res.on('end', function () {
-      file.end();
+export function download(url: string, dist: string) {
+  return new Promise<null>((resolve, reject) => {
+    const get = url.startsWith('https://') ? https.get : http.get;
+    get(url, async (res) => {
+      if (res.statusCode && res.statusCode >= 200 && res.statusCode < 300) {
+        const file = createWriteStream(dist);
+        await mkdir(dirname(dist), { recursive: true });
+        res.pipe(file, { end: false });
+        res.on('end', function () {
+          file.end();
+          resolve(null);
+        });
+      } else if (
+        res.statusCode &&
+        res.statusCode >= 300 &&
+        res.statusCode < 400 &&
+        res.headers.location
+      ) {
+        const newUrl = res.headers.location;
+        download(newUrl, dist).then(resolve).catch(reject);
+      } else {
+        reject(new Error('Failed to download ' + url));
+      }
     });
   });
 }
